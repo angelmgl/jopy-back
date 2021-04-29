591864e6522526f519275c1a165bdf691954b72f
Angel Mgl <angelemegeele@gmail.com>
2021-04-29 15:49:11 -0400

    feat: Add login route and controller with token authentication

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,47 +1,91 @@
 const jwt = require("jsonwebtoken");
 const { SECRET } = require("../config");
 
-const { encryptPassword, matchPasswords } = require("../libs/helpers");
+const {
+    encryptPassword,
+    matchPasswords,
+    searchUsername,
+} = require("../libs/helpers");
 const db = require("../database");
 
 const controllers = {
     signup: async (req, res) => {
         const { username, password, fullname } = req.body;
 
-        const newUser = { 
-            username, 
+        const newUser = {
+            username,
             password: await encryptPassword(password),
-            fullname 
+            fullname,
         };
 
-        const checkUsername = await db.query("SELECT Count(*) FROM users WHERE username = ?", [username]);
+        const checkUsername = await searchUsername(username);
 
-        // check if the username already exists 
-        if (Object.values(checkUsername[0]) == 0) {
+        // check if the username already exists
+        if (checkUsername == 0) {
             try {
-                const savedUser = await db.query("INSERT INTO users set ?", [newUser]);
+                const savedUser = await db.query("INSERT INTO users set ?", [
+                    newUser,
+                ]);
                 // create a new token for the user
                 const token = jwt.sign(
                     { id: savedUser.insertId },
                     SECRET,
                     { expiresIn: 604800 } // 7 days
-                    );
+                );
 
                 res.status(201).json({
+                    success: true,
                     id: savedUser.insertId,
-                    token
+                    token,
                 });
             } catch (error) {
                 console.log(error);
             }
         } else {
-            res.status(400).json("The username " + username + " already exist")
+            res.status(400).json({
+                success: false,
+                message: "The username " + username + " already exist",
+                token: null
+            });
         }
     },
 
     login: async (req, res) => {
-        res.json("login");
+        const { username, password } = req.body;
+
+        const user = await db.query("SELECT * FROM users WHERE username = ?", [
+            username,
+        ]);
+
+        // if the user does'nt exist...
+        if (!user[0]) {
+            res.status(400).json({
+                success: false,
+                message: "The username " + username + " does'nt exist",
+                token: null,
+            });
+        } else {
+            const match = await matchPasswords(password, user[0].password);
+            // compare passwords
+            if (match) {
+                console.log(user[0]);
+                const token = jwt.sign({ id: user[0].id }, SECRET, {
+                    expiresIn: 604800,
+                });
+                res.status(200).json({
+                    success: true,
+                    id: user[0].id,
+                    token,
+                });
+            } else {
+                res.status(401).json({
+                    success: false,
+                    message: "Wrong password",
+                    token: null,
+                });
+            }
+        }
     },
-}
+};
 
 module.exports = controllers;
--- libs/helpers.js
+++ libs/helpers.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const db = require("../database");
 
 const helpers = {};
 
@@ -16,6 +17,20 @@ helpers.matchPasswords = async (password, receivedPassword) => {
     try {
         const res = await bcrypt.compare(password, receivedPassword);
         return res; // returns a boolean
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+helpers.searchUsername = async (username) => {
+    try {
+        // search if already exist that username in the database
+        const checkUsername = await db.query(
+            "SELECT Count(*) FROM users WHERE username = ?",
+            [username]
+        );
+        // return 1 or 0
+        return Object.values(checkUsername[0]);
     } catch (error) {
         console.log(error);
     }

ef56ba14ffd6b0025ae6ddee4fce6268b7eabc86
Angel Mgl <angelemegeele@gmail.com>
2021-04-29 14:27:19 -0400

    feat: Add signup route and controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,31 @@
+const jwt = require("jsonwebtoken");
+
 const { encryptPassword, matchPasswords } = require("../libs/helpers");
+const db = require("../database");
 
 const controllers = {
     signup: async (req, res) => {
         const { username, password, fullname } = req.body;
-        
-        console.log(req.body);
+
+        const newUser = { 
+            username, 
+            password: await encryptPassword(password),
+            fullname 
+        };
+
+        const checkUsername = await db.query("SELECT Count(*) FROM users WHERE username = ?", [username]);
+
+        // check if the username already exists 
+        if (Object.values(checkUsername[0]) == 0) {
+            try {
+                const savedUser = await db.query("INSERT INTO users set ?", [newUser]);
+                res.status(201).json({id: savedUser.insertId});
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            res.status(400).json("The username " + username + " already exist")
+        }
     },
 
     login: async (req, res) => {

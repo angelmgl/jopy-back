cfdff610dec3d78be2a1ca6cdbaa696e0ab46e66
Angel Mgl <angelemegeele@gmail.com>
2021-04-30 00:33:56 -0400

    refactor: Handle promises with trycatch

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -12,76 +12,86 @@ const controllers = {
     signup: async (req, res) => {
         const { username, password, fullname } = req.body;
 
-        const newUser = {
-            username,
-            password: await encryptPassword(password),
-            fullname,
-        };
+        try {
+            const newUser = {
+                username,
+                password: await encryptPassword(password),
+                fullname,
+            };
 
-        const checkUsername = await searchUsername(username);
+            const checkUsername = await searchUsername(username);
 
-        // check if the username already exists
-        if (checkUsername == 0) {
-            try {
-                const savedUser = await db.query("INSERT INTO users SET ?", [
-                    newUser,
-                ]);
-                // create a new token for the user
-                const token = jwt.sign(
-                    { id: savedUser.insertId },
-                    SECRET,
-                    { expiresIn: 604800 } // 7 days
-                );
+            // check if the username already exists
+            if (checkUsername == 0) {
+                try {
+                    const savedUser = await db.query(
+                        "INSERT INTO users SET ?",
+                        [newUser]
+                    );
+                    // create a new token for the user
+                    const token = jwt.sign(
+                        { id: savedUser.insertId },
+                        SECRET,
+                        { expiresIn: 604800 } // 7 days
+                    );
 
-                res.status(201).json({
-                    success: true,
-                    token,
+                    res.status(201).json({
+                        success: true,
+                        token,
+                    });
+                } catch (error) {
+                    console.log(error);
+                }
+            } else {
+                res.status(400).json({
+                    success: false,
+                    message: "The username " + username + " already exist",
+                    token: null,
                 });
-            } catch (error) {
-                console.log(error);
             }
-        } else {
-            res.status(400).json({
-                success: false,
-                message: "The username " + username + " already exist",
-                token: null,
-            });
+        } catch (error) {
+            console.log(error);
         }
     },
 
     login: async (req, res) => {
         const { username, password } = req.body;
 
-        const user = await db.query("SELECT * FROM users WHERE username = ?", [
-            username,
-        ]);
+        try {
+            const user = await db.query(
+                "SELECT * FROM users WHERE username = ?",
+                [username]
+            );
 
-        // if the user does'nt exist...
-        if (!user[0]) {
-            res.status(400).json({
-                success: false,
-                message: "The username " + username + " does'nt exist",
-                token: null,
-            });
-        } else {
-            const match = await matchPasswords(password, user[0].password);
-            // compare passwords
-            if (match) {
-                console.log(user[0]);
-                const token = jwt.sign({ id: user[0].id }, SECRET, {
-                    expiresIn: 604800,
-                });
-                res.status(200).json({
-                    success: true,
-                    token,
-                });
-            } else {
-                res.status(401).json({
+            // if the user does'nt exist...
+            if (!user[0]) {
+                res.status(400).json({
                     success: false,
-                    message: "Wrong password",
+                    message: "The username " + username + " does'nt exist",
                     token: null,
                 });
+            } else {
+                const match = await matchPasswords(password, user[0].password);
+                // compare passwords
+                if (match) {
+                    console.log(user[0]);
+                    const token = jwt.sign({ id: user[0].id }, SECRET, {
+                        expiresIn: 604800,
+                    });
+                    res.status(200).json({
+                        success: true,
+                        token,
+                    });
+                } else {
+                    res.status(401).json({
+                        success: false,
+                        message: "Wrong password",
+                        token: null,
+                    });
+                }
             }
+        } catch (error) {
+            console.log(error);
         }
     },
 };
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("Welcome to Jopy API!");
+    res.send("Welcome to Jopy API! See the documentation here: https://github.com/angelmgl/jopy-back");
 });
 
 module.exports = router;
